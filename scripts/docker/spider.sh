dc27bab64719ece21a48f5a9401b4c2c21e53535
selfdanielj <38377823+selfdanielj@users.noreply.github.com>
2025-09-26 15:19:03 -0400

    SRCH-6225 Run spider in docker (#334)
    * SRCH-6225 add spider section for docker
    
    * SRCH-6225 update benchmark defaults
    
    * SRCH-6225 add components to help run with search-services and in this repo
    
    * SRCH-6225 tweaks to version, script, and docs
    
    * SRCH-6225 tweak version, remove prints
    
    * SRCH-6225 update adhoc docker instructions
    
    * SRCH-6225 make PR improvements

---
+++ scripts/docker/spider.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+# A script to allow for simple spider usage in docker.
+
+DOCKER_WORK_DIR=/usr/src/searchgov-spider
+
+_show_help() {
+    echo "crawl - A simple interface allowing for ad hoc spider runs."
+    echo ""
+    echo "Usage: crawl <domain> <starting-url> [OPTIONS]"
+    echo ""
+    echo "Required Arguments:"
+    echo "domain:           Domain used to limit crawls"
+    echo "starting-url:     URL to start crawling at"
+    echo ""
+    echo "Options:"
+    echo "  --help           Display this help message"
+    echo ""
+    echo "Examples:"
+    echo "   Crawl starting at https://www.gsa.gov, limited to links that are in the www.gsa.gov domain"
+    echo "     - crawl www.gsa.gov https://www.gsa.gov"
+}
+
+_has_args() {
+    if [ "$#" -lt 2 ]; then
+        echo "ERROR: Not Enough Arguments!"
+        echo ""
+        _show_help
+        return 1
+    fi
+}
+
+crawl() {
+    (_has_args $@) || exit 1
+
+    ALLOWED_DOMAINS=$1
+    STARTING_URLS=$2
+    shift 2
+
+    cd $DOCKER_WORK_DIR/search_gov_crawler
+    python benchmark.py -d $ALLOWED_DOMAINS -u $STARTING_URLS $@
+}
+
+"$@"
