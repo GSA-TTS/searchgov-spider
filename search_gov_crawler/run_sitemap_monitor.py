8f3fb3b5f68faf7c24df22989d9ecb03753d27c1
Daniel Self <self.danielj@gmail.com>
2025-10-09 11:25:17 -0400

    SRCH-6206 move crawl sites import to sitemap monitor

--- search_gov_crawler/run_sitemap_monitor.py
+++ search_gov_crawler/run_sitemap_monitor.py
@@ -1,10 +1,10 @@
 import logging
 import os
+from pathlib import Path
 
 from dotenv import load_dotenv
 from pythonjsonlogger.json import JsonFormatter
 
-from search_gov_crawler.scrapy_scheduler import CRAWL_SITES_FILE
 from search_gov_crawler.search_gov_app.crawl_config import CrawlConfigs
 from search_gov_crawler.search_gov_spiders.extensions.json_logging import LOG_FMT
 from search_gov_crawler.search_gov_spiders.sitemaps.sitemap_monitor import SitemapMonitor
@@ -14,6 +14,10 @@ load_dotenv()
 logging.basicConfig(level=os.environ.get("SCRAPY_LOG_LEVEL", "INFO"))
 logging.getLogger().handlers[0].setFormatter(JsonFormatter(fmt=LOG_FMT))
 log = logging.getLogger("search_gov_crawler.run_sitemap_monitor")
+
+CRAWL_SITES_FILE = (
+    Path(__file__).parent / "domains" / os.environ.get("SPIDER_CRAWL_SITES_FILE_NAME", "crawl-sites-production.json")
+)
 
 if __name__ == "__main__":
     log.info("Starting Sitemap Monitor...")
