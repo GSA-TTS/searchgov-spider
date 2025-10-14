6332c0e126903209bfce1ecf8800d48ff7fef660
selfdanielj <38377823+selfdanielj@users.noreply.github.com>
2025-10-14 15:24:11 -0400

    SRCH-6208 Load JSON to DB (#346)
    * SRCH-6208 add pymysql to dependencies
    
    * SRCH-6208 add search_gov_app module and database connectivity
    
    * SRCH-6208 create script to load records into crawl config table
    
    * SRCH-6208 add mysql to spider docker
    
    * SRCH-6208 update help
    
    * SRCH-6208 add test for db connection
    
    * SRCH-6208 include DB env vars in fetch script
    
    * SRCH-6208 fix typo in requirements file
    
    * SRCH-6208 provide better sample values in .env sample file
    
    * SRCH-6208 update path for load script
    
    * SRCH-6208 address code feedback

--- scripts/README.md
+++ scripts/README.md
@@ -3,8 +3,9 @@
 This folder contains miscellaneous scripts that help us perform reoccurring but sporadic tasks in a controlled and repeatable manner.
 
 ## Contents
-* `cache_tools.py`      - Allows for insight and action on the redis cache.
-* `convert_libsonnet/`  - Assists in converting large numbers of domains in csv format to jsonnet.
-* `delete_by_domain.py` - Deletes all records in the spider index based on the name of the domain given.
-* `query_tester.py`     - Used to help acceptance test changes to the search query
-* `sitemaps_sandbox/`   - Included scripts to help in testing of sitemap functionality.
+* `cache_tools.py`        - Allows for insight and action on the redis cache.
+* `convert_libsonnet/`    - Assists in converting large numbers of domains in csv format to jsonnet.
+* `delete_by_domain.py`   - Deletes all records in the spider index based on the name of the domain given.
+* `load_crawl_configs.py` - Helper to load crawl site JSON files to crawl config table.
+* `query_tester.py`       - Used to help acceptance test changes to the search query
+* `sitemaps_sandbox/`     - Included scripts to help in testing of sitemap functionality.
---
+++ tests/search_gov_app/test_database.py
@@ -0,0 +1,48 @@
+import pytest
+from pymysql.cursors import DictCursor
+
+from search_gov_crawler.search_gov_app.database import get_database_connection
+
+
+class MockConnection:
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+
+    def close(self):
+        return True
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        return None
+
+
+def test_get_database_connection_missing_password(monkeypatch):
+    monkeypatch.delenv("DB_PASSWORD", raising=False)
+    with pytest.raises(ValueError, match="DB_PASSWORD environment variable must be set"):
+        get_database_connection().__enter__()
+
+
+def test_get_database_connection(monkeypatch):
+    monkeypatch.setenv("DB_HOST", "test_host")
+    monkeypatch.setenv("DB_PORT", "1234")
+    monkeypatch.setenv("DB_NAME", "test_db")
+    monkeypatch.setenv("DB_USER", "test_user")
+    monkeypatch.setenv("DB_PASSWORD", "test_password")
+
+    def mock_connect(**kwargs):
+        return MockConnection(**kwargs)
+
+    monkeypatch.setattr("search_gov_crawler.search_gov_app.database.connect", mock_connect)
+
+    with get_database_connection() as connection:
+        assert connection.kwargs == {
+            "host": "test_host",
+            "port": 1234,
+            "database": "test_db",
+            "user": "test_user",
+            "password": "test_password",
+            "cursorclass": DictCursor,
+            "charset": "utf8mb4",
+        }
