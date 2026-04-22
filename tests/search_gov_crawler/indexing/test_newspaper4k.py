a11c05e4bb253e81b0ac6b5dee9401e81ef43b2f
Daniel Self <self.danielj@gmail.com>
2026-04-22 10:00:16 -0400

    SRCH-6504 improve test coverage

--- tests/search_gov_crawler/indexing/test_newspaper4k.py
+++ tests/search_gov_crawler/indexing/test_newspaper4k.py
@@ -1,33 +1,20 @@
-from pathlib import Path
-
 from newspaper import Article
 
 from search_gov_crawler.indexing.parse import convert_html_scrapy
 
 
-def load_file_with_pathlib(filename):
-    script_dir = Path(__file__).resolve().parent
-    file_path = script_dir / filename
-    try:
-        return file_path.read_text()
-    except FileNotFoundError:
-        return f"Error: File '{filename}' not found."
-
-
-def test_newspaper4k_failed_html_parsing():
+def test_newspaper4k_failed_html_parsing(test_scrapy_html_1):
     """Test newspaper4k failed article text extraction from an HTML file."""
-    html_content = load_file_with_pathlib("test_scrapy_html_1.html")
     article = Article(url="http://example.gov")
-    article.download(input_html=html_content)
+    article.download(input_html=test_scrapy_html_1)
     article.parse()
     article.nlp()
     assert len(article.text) == 0
 
 
-def test_convert_scrapy_successful_html_parsing():
+def test_convert_scrapy_successful_html_parsing(test_scrapy_html_1):
     """Test convert_html_scrapy successful article text extraction from an HTML file."""
-    html_content = load_file_with_pathlib("test_scrapy_html_1.html")
-    result = convert_html_scrapy(html_content)
+    result = convert_html_scrapy(test_scrapy_html_1)
     assert isinstance(result, dict)
     assert "content" in result
     assert "title" in result
