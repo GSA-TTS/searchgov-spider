dd782eec2dbeb55378522906bff08d764c470114
Daniel Self <self.danielj@gmail.com>
2026-07-27 12:50:58 -0400

    SRCH-6634 update schedule.  un-comment stop and start scripts

--- cicd-scripts/app_stop.sh
+++ cicd-scripts/app_stop.sh
@@ -151,8 +151,7 @@ remove_venv
 purge_pip_cache
 
 # Stop freshness checker
-# SRCH-6633 disabled so we can manually run and delete documents
-# stop_freshness_checker
+stop_freshness_checker
 
 # Stop scrapy scheduler if running
 stop_scrapy_scheduler
