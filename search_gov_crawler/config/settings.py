86a98aed20ac22d2b75adb32547e385c96653ae9
Daniel Self <self.danielj@gmail.com>
2026-07-27 11:25:17 -0400

    SRCH-6505 add new settings class

---
+++ search_gov_crawler/config/settings.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class SearchgovSpiderSettings(BaseSettings):
+    """Global Settings Class That reads from environment or .env file"""
+
+    scrapy_log_level: str = "INFO"
+    opensearch_search_host: str = "http://localhost:9200"
+    opensearch_search_index: str = "development-i14y-documents-searchgov"
+    opensearch_search_user: str = ""
+    opensearch_search_password: str = ""
+
+    model_config = SettingsConfigDict(
+        case_sensitive=True,
+        dotenv_filtering="only_existing",
+        env_file=(Path(__file__).parent.parent.parent / ".env"),
+        env_file_encoding="utf-8",
+    )
